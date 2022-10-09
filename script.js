897afa2fabf36ef84bfade1e407fb167d9cb6523
Shayan <54780443+jahanzebshayan@users.noreply.github.com>
2022-10-09 17:19:44 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name        wikipedia table header float
+// @namespace   Violentmonkey Scripts
+// @grant       none
+// @version     1.0
+// @author      tanhaworen
+// @description 2/8/2022, 6:40:34 PM
+// @match      https://*.wikipedia.org/wiki/*
+// @run-at     document-start
+// @license      GNU GPLv2
+// ==/UserScript==
+
+
+document.addEventListener('DOMNodeInserted', function() {
+  var listOfThead = document.getElementsByTagName('thead');
+  //console.log(listOfThead)
+  var num = listOfThead.length;
+  //console.log(num)
+  var newUI = document.getElementById('vector-sticky-header');
+    if (num > 0 ){
+    Array.from(listOfThead).forEach(child => {
+      child.style.position = "sticky";
+      if(newUI){
+          child.style.top = "50px";
+      } else {
+          child.style.top = 0;
+      }
+    });
+  }
+}, false);
